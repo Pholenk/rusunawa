bf6ab13cf19f4a2f544f4e1f2210994f05273de4
Pholenk <pholenkadi17@gmail.com>
2017-09-30 10:29:49 +0700

    initialize project

---
+++ application/views/tampilan_content.php
@@ -0,0 +1,15 @@
+<div class="alert alert-block alert-success">
+									<button type="button" class="close" data-dismiss="alert">
+										<i class="ace-icon fa fa-times"></i>
+									</button>
+
+									<i class="ace-icon fa fa-check green"></i>
+
+									Selamat Datang, 
+									<strong class="green">
+										Sistem E-Rusunawa
+										<small>(v1)</small>
+									</strong>,
+	Dinas Komunikasi dan Informatika DIY - Bidang Manajemen Informatika
+	<a href="https://github.com/bopoda/ace">DISKOMINDFO</a> DIY
+								</div>
---
+++ application/views/tampilan_menu.php
@@ -0,0 +1,146 @@
+<ul class="nav nav-list">
+					<li class="active">
+						<a href="index.html">
+							<i class="menu-icon fa fa-tachometer"></i>
+							<span class="menu-text"> Dashboard </span>
+						</a>
+
+						<b class="arrow"></b>
+					</li>
+
+					<li class="">
+						<a href="#" class="dropdown-toggle">
+							<i class="menu-icon fa fa-desktop"></i>
+							<span class="menu-text">
+								Data Master
+							</span>
+
+							<b class="arrow fa fa-angle-down"></b>
+						</a>
+
+						<b class="arrow"></b>
+
+						<ul class="submenu">
+							<li class="">
+								<a href="<?php echo base_url(); ?>index.php/penghuni">
+									<i class="menu-icon fa fa-caret-right"></i>
+
+									Data Penghuni
+									<b class="arrow fa fa-angle-down"></b>
+								</a>
+
+								<b class="arrow"></b>
+
+								</ul>
+								<b class="arrow"></b>
+
+						<ul class="submenu">
+							<li class="">
+								<a href="<?php echo base_url(); ?>index.php/kamar">
+									<i class="menu-icon fa fa-caret-right"></i>
+
+									Data Kamar
+									<b class="arrow fa fa-angle-down"></b>
+								</a>
+
+								<b class="arrow"></b>
+
+								</ul>
+							</li>
+
+						
+							
+
+					
+					<li class="">
+						<a href="#" class="dropdown-toggle">
+							<i class="menu-icon fa fa-pencil-square-o"></i>
+							<span class="menu-text"> Transaksi </span>
+
+							<b class="arrow fa fa-angle-down"></b>
+						</a>
+					
+
+						<b class="arrow"></b>
+
+						<ul class="submenu">
+							<li class="">
+								<a href="<?php echo base_url(); ?>index.php/transaksi">
+									<i class="menu-icon fa fa-caret-right"></i>
+									Sewa
+								</a>
+
+								<b class="arrow"></b>
+							</li>
+
+							<li class="">
+								<a href="<?php echo base_url(); ?>index.php/kontrak">
+									<i class="menu-icon fa fa-caret-right"></i>
+									Lihat Kontrak
+								</a>
+
+								<b class="arrow"></b>
+							</li>
+						</ul>
+					</li>
+
+					
+					<li class="">
+						<a href="#" class="dropdown-toggle">
+							<i class="menu-icon fa fa-list-alt"></i>
+							<span class="menu-text"> Laporan </span>
+						</a>
+
+						<b class="arrow fa fa-angle-down"></b>
+
+						<b class="arrow fa fa-angle-down"></b>
+
+						<ul class="submenu">
+							<li class="">
+								<a href="<?php echo base_url(); ?>index.php/penghuni/render">
+									<i class="menu-icon fa fa-caret-right"></i>
+									Laporan Data Penghuni
+								</a>
+
+								<b class="arrow"></b>
+							</li>
+
+							<li class="">
+								<a href="<?php echo base_url(); ?>index.php/transaksi/render">
+									<i class="menu-icon fa fa-caret-right"></i>
+									Laporan Data Sewa Rusunawa
+								</a>
+
+								<b class="arrow"></b>
+							</li>
+						</ul>
+					</li>
+
+					<li class="">
+						<a href="<?php echo base_url(); ?>index.php/calendar.google.com">
+							<i class="menu-icon fa fa-calendar"></i>
+
+							<span class="menu-text">
+								Calendar
+
+								
+								
+								</span>
+							</span>
+						</a>
+
+						<b class="arrow"></b>
+					</li>
+
+					<li class="">
+						<a href="gallery.html">
+							<i class="menu-icon fa fa-picture-o"></i>
+							<span class="menu-text"> Gallery </span>
+						</a>
+
+						<b class="arrow"></b>
+					</li>
+
+						</ul>
+					</li>
+				</ul><!-- /.nav-list -->
